db5de83b072e291f8e2e829f2000da3dae78b742
Toni Cebrián <toni.cebrian@gmail.com>
2017-03-21 11:10:42 +0100

    Changes to use deim widget

--- runme-first.sh
+++ runme-first.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | sh

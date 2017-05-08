ec6fcf9c4b923890168cbb9db32f202ddf667fb9
Toni Cebrián <toni.cebrian@gmail.com>
2017-05-08 12:46:52 +0200

    Fix usage of dein

--- runme-first.sh
+++ runme-first.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | sh
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > /tmp/installer.sh
+sh /tmp/installer.sh ~/.config/nvim/

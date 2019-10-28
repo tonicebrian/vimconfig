b6a264073959223214a7b6b495e180e5fc2ff89a
Toni Cebrián <toni.cebrian@dowjones.com>
2019-10-28 14:38:38 +0100

    Don't pollute local folder

--- runme-first.sh
+++ runme-first.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > /tmp/installer.sh
 # For example, we just use `~/.cache/dein` as installation directory
-sh ./installer.sh ~/.cache/dein
+sh /tmp/installer.sh ~/.cache/dein

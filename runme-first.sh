39183d18ca5110efc3923e7b4f4ba43dc9680341
Toni Cebrián <toni.cebrian@dowjones.com>
2019-10-28 14:54:02 +0100

    Latest install instructions

--- runme-first.sh
+++ runme-first.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > /tmp/installer.sh
-# For example, we just use `~/.cache/dein` as installation directory
-sh /tmp/installer.sh ~/.cache/dein
+# For example, we just use `~/.config/nvim/dein` as installation directory
+sh /tmp/installer.sh ~/.config/nvim/dein
